5419eab65c9581a0feb03be0986152f99f5ba481
MrGoal <boroznata1984@gmail.com>
2025-06-02 14:48:23 +0300

    настройка maven и полный перелопат всего живого(когда то)

--- db-core/build.gradle.kts
+++ db-core/build.gradle.kts
@@ -3,17 +3,17 @@ plugins {
 }
 
 dependencies {
-    api(project(":db-core"))
-    api(libs.google.guice)
-
+    api(libs.slf4j.api)
+    api("jakarta.inject:jakarta.inject-api:2.0.1")
     runtimeOnly(libs.logback.classic)
 
     testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.logback.classic)
     testImplementation(libs.junit.jupiter)
     testImplementation("org.mockito:mockito-core:5.17.0")
+    testImplementation("com.h2database:h2:2.3.232")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
-
